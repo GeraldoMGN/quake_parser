e79fb6c46877ee70fedec17c88e6fd0b4ec61524
Geraldo Galvao <gmgn@cin.ufpe.br>
2021-02-27 22:56:53 -0300

    Current token getter and add comments

--- src/gameLogParser.js
+++ src/gameLogParser.js
@@ -1,11 +1,21 @@
+/*   The parser works breaking the input into tokens (strings with no spaces), then starts
+ *  iterating through the tokens and consuming then, reading it's value to handle what should
+ *  be added to the gameInfo object;
+ */
 class GameLogParser {
   constructor(log) {
     this.log = log;
-    this.tokenIndex = 0;
+
     this.logTokenized = [];
+    this.tokenIndex = 0;
+
     this.kills = [];
 
     this.parse();
+  }
+
+  get currentToken() {
+    return this.logTokenized[this.tokenIndex];
   }
 
   parse() {
@@ -20,8 +30,9 @@ class GameLogParser {
     return inputString.split(' ');
   }
 
+  // Handles and consumes current token
   consumeToken() {
-    switch (this.logTokenized[this.tokenIndex]) {
+    switch (this.currentToken()) {
       case 'Kill:': {
         this.consumeKillToken();
         break;
@@ -30,23 +41,25 @@ class GameLogParser {
     }
   }
 
-  consumeTokenUntil(endToken) {
+  // Consumes tokens until finds endToken
+  consumeTokensUntil(endToken) {
     const consumedTokens = [];
-    while (this.logTokenized[this.tokenIndex] !== endToken) {
-      if (this.logTokenized[this.tokenIndex] !== endToken) {
-        consumedTokens.push(this.logTokenized[this.tokenIndex]);
+    while (this.currentToken() !== endToken) {
+      if (this.currentToken() !== endToken) {
+        consumedTokens.push(this.currentToken());
         this.tokenIndex += 1;
       }
     }
     return consumedTokens;
   }
 
+  // Consumes "Killed:"
   consumeKillToken() {
     this.tokenIndex += 4;
-    const killerPlayer = this.consumeTokenUntil('killed').join(' ');
+    const killerPlayer = this.consumeTokensUntil('killed').join(' ');
 
     this.tokenIndex += 1;
-    const killedPlayer = this.consumeTokenUntil('by').join(' ');
+    const killedPlayer = this.consumeTokensUntil('by').join(' ');
 
     this.kills.push({
       killer: killerPlayer,
