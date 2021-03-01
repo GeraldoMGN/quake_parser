a76928c6177a8670d9b9d3f9000bc1a30d9f77c5
Geraldo Galvao <gmgn@cin.ufpe.br>
2021-03-01 09:46:38 -0300

    gameByID response includes player kill ranking

--- src/gameAPI.js
+++ src/gameAPI.js
@@ -3,34 +3,49 @@ import GameLogParser from './gameLogParser.js';
 import { readJSON, saveJSON } from './utils.js';
 
 class GameAPI {
-  constructor(games) {
-    this.games = games;
+  constructor() {
+    this._games = null;
+    this._playerRanking = [];
+  }
+
+  get games() {
+    return this._games;
+  }
+
+  get playerRanking() {
+    return this._playerRanking;
   }
 
   readLog(filename) {
     const data = readJSON(filename);
     const parser = new GameLogParser(data);
-    this.games = parser.parse();
-    saveJSON(this.games, 'games');
+    this._games = parser.parse();
+    this._rankPlayers();
+
+    saveJSON(this._games, 'games');
+  }
+
+  _rankPlayers() {
+    this._games.forEach((game) => {
+      // Ranks players by number of kills
+      const { kills } = Object.values(game)[0];
+      this.playerRanking.push(Object.entries(kills).sort(
+        (entry1, entry2) => entry2[1] - entry1[1],
+      ));
+    });
   }
 
   // This is the solution to task 2
   printRanking() {
-    if (!this.games) {
+    if (!this._games) {
       throw Error('You need to read a game log before printing the player ranking');
     }
 
-    this.games.forEach((game) => {
-      console.log(`${Object.keys(game)[0]}, ranked by kills:`);
-
-      // Ranks players by number of kills
-      const { kills } = Object.values(game)[0];
-      const rankedPlayers = Object.entries(kills).sort(
-        (entry1, entry2) => entry2[1] - entry1[1],
-      );
+    this.playerRanking.forEach((gameRanking, gameIndex) => {
+      console.log(`Game ${gameIndex}, ranked by kills:`);
 
       // Prints ranking
-      rankedPlayers.forEach((entry) => (
+      gameRanking.forEach((entry) => (
         console.log(`  ${entry[0]}: ${entry[1]} kills`)));
       console.log('');
     });
@@ -38,7 +53,7 @@ class GameAPI {
 
   // This is the solution to task 3
   startServer(port) {
-    if (!this.games) {
+    if (!this._games) {
       throw Error('You need to read a game log before starting game API server');
     }
 
@@ -47,8 +62,14 @@ class GameAPI {
     // Get game information by id
     app.get('/gameByID/:gameID', (req, res) => {
       const { gameID } = req.params;
-      if (gameID > 0 && gameID < this.games.length) {
-        res.status(200).json(this.games[gameID]);
+      if (gameID > 0 && gameID < this._games.length) {
+        // Inserts the payer kill ranking to the response object
+        const gameInfoWithRanking = {
+          ...this._games[gameID],
+          ranking: this.playerRanking[gameID],
+        };
+
+        res.status(200).json(gameInfoWithRanking);
       } else {
         res.status(404).send('ID provided does not represent a game.');
       }
